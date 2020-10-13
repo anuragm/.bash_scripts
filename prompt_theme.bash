b83cfdfd3711c9f6df983bcfc7d47fb062b2a31e
Anurag Mishra <mishra.anurag07@gmail.com>
2020-10-12 18:00:29 -0700

    Bug fix Git status
    Use the porcelain for better compatability

--- prompt_theme.bash
+++ prompt_theme.bash
@@ -74,7 +74,7 @@ shortpath () {
 }
 
 parse_git_dirty () {
-  [[ $(git status 2> /dev/null | tail -n1) != "nothing to commit (working directory clean)" ]] && echo "*"
+  [ ! -z "$(git status --porcelain 2> /dev/null)" ] && echo '*'
 }
 
 parse_git_branch () {
