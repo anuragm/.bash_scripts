371a8ed67202907cdad94b377f2a69b81d454380
Anurag Mishra <mishra.anurag07@gmail.com>
2021-04-30 13:30:35 -0700

    Use natural sort order for ls

--- aliases.bash
+++ aliases.bash
@@ -1,5 +1,5 @@
 ## Aliases ##
-alias ls='gls --color=auto -FhX --group-directories-first'
+alias ls='gls --color=auto -FhXv --group-directories-first'
 alias ec='/usr/local/bin/emacsclient -t'
 alias rstm='printf '"'"'\e[?1000l'"'" # Reset mouse
 alias pdfl="pdflatex -interaction=batchmode -synctex=1"
